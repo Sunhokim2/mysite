418c71df59a0e8d843bd73bdf33cb69878879073
Sunhokim2 <mcwright@naver.com>
2025-01-21 18:21:32 +0900

    데이터베이스 sqlite -> MYSQL 변경.
    스키마NAME은 생성안해서 오류생길수도 있음

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,13 +80,25 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "HOST" : "13.124.121.125",
+
+        "NAME": "youtube_devops",
+
+        "USER": "root",
+        "PASSWORD": "password",
+        "PORT": "3306",
+        "OPTIONS": {"charset": "utf8mb4"}
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
