60f1dc369426af2321cba1ca26882930a2879c99
Sunhokim2 <mcwright@naver.com>
2025-02-01 12:26:42 +0900

    ..

--- content/views.py
+++ content/views.py
@@ -46,7 +46,8 @@ class Main(APIView):
                                   nickname=feed_user.nickname,
                                   reply_list=reply_list,
                                   is_liked=is_liked,
-                                  is_marked=is_marked
+                                  is_marked=is_marked,
+                                  email = feed.email
                                   ))
         user = User.objects.filter(email=email).first()
 
